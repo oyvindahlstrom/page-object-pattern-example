b5792e460f2fc8afd61271d3528eec9526c58020
Øyvind Ahlstrøm <oyvind.ahlstrom@gmail.com>
2023-03-07 20:39:41 +0100

    Refactored HomePageObject in TS

--- todo-app/src/test/pageobjects/HomePageObject.ts
+++ todo-app/src/test/pageobjects/HomePageObject.ts
@@ -4,43 +4,32 @@ import EditPageObject from "./EditPageObject";
 
 class HomePageObject {
 
-  private readonly tableBodyRows?: HTMLElement[];
-  private readonly addNewTodoButton?: HTMLElement;
+  private readonly tableBodyRows: HTMLElement[];
+  private readonly addNewTodoButton: HTMLElement;
 
   constructor() {
     this.tableBodyRows = screen.getAllByRole('row').slice(1); // Removes the table header row
     this.addNewTodoButton = screen.getByRole('button', {name: 'Add new TODO'});
+    this.verifyExistenceOfElements();
   }
 
   public countTodos(count: number) {
-    if (!this.tableBodyRows) {
-      fail('The todo-table has not been initialized');
-    }
     expect(this.tableBodyRows.length).toBe(count);
     return this;
   }
 
   public verifyTodoDescriptionInTable(rowNumber: number, expectedDescription: string) {
-    if (!this.tableBodyRows) {
-      fail('The todo-table has not been initialized');
-    }
     expect(this.descriptionFieldAtRowNumber(rowNumber)).toHaveTextContent(expectedDescription);
     return this;
   }
   
   // This method is a little less precise but it will still verify if the text is present in the DOM
   public alternativeVerifyDescriptionInTable(expectedDescription: string) {
-    if (!this.tableBodyRows) {
-      fail('The todo-table has not been initialized');
-    }
     expect(screen.getByText(expectedDescription)).toBeInTheDocument();
     return this;
   }
 
   public clickOnAddNewTodo() {
-    if (!this.addNewTodoButton) {
-      fail('The add new todo button has not been initialized');
-    }
     fireEvent.click(this.addNewTodoButton);
     return new AddPageObject();
   }
@@ -83,6 +72,15 @@ class HomePageObject {
 
   private editButtonAtRowNumber(rowNumber: number) {
     return this.tableBodyRows?.[rowNumber].children[3].firstChild;
+  }
+
+  private verifyExistenceOfElements() {
+    if (!this.tableBodyRows) {
+      fail('The todo-table has not been initialized');
+    }
+    if (!this.addNewTodoButton) {
+      fail('The add new todo button has not been initialized');
+    }
   }
 
 };
